17d5ffdd61e329f2c92b1310cb4c257c1f074e80
sunckell <sunckell@gmail.com>
2015-11-12 09:18:41 -0500

    update crumbs files with proper name step42...  Originally named step52...

--- step42WEB/src/main/groovy/us/kellerman/step42/utility/connectivity/SecureSessionUtility.groovy
+++ step42WEB/src/main/groovy/us/kellerman/step42/utility/connectivity/SecureSessionUtility.groovy
@@ -1,7 +1,10 @@
 package us.kellerman.step42.utility.connectivity
 
+import static com.aestasit.infrastructure.ssh.DefaultSsh.*
+
 /**
  * Created by ckell on 11/9/15.
  */
 class SecureSessionUtility {
+
 }
